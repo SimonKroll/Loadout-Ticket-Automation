76b8740f4b35a05bc281174358efd93156a2010e
SimonKroll <32710261+SimonKroll@users.noreply.github.com>
2024-02-25 16:09:04 -0600

    Added check for non-blocking ID read

--- main.py
+++ main.py
@@ -39,64 +39,65 @@ reader = SimpleMFRC522()
 while True:
     try:
         id, text = reader.read_no_block()
-        buzzer.beep()
+        
+        if id:
+            buzzer.beep()
+
+            cursor.execute("SELECT * FROM credentials WHERE CardID=?", [int(id)]) 
+            row = cursor.fetchone()
+
+            if row: # Ensures card is on file
+                # Update & Retrieve the Ticket Counter
+                cursor.execute("UPDATE counters SET value = value + 1 WHERE counter_name = 'loadID'") # TODO: Rename loadID to ticket
+                conn.commit() 
+                cursor.execute("SELECT value FROM counters WHERE counter_name = 'loadID';")
+                ticket_number = str(cursor.fetchone()[0]).zfill(5)
+
+                # Update & Retrieve the Contract Load Counter
+                # TODO: initialize contract counters on startup
+                cursor.execute('''INSERT OR IGNORE INTO counters
+                                VALUES (?, 0)''', [row[6]])
+                conn.commit() 
+
+                cursor.execute("UPDATE counters SET value = value + 1 WHERE counter_name = ?", [row[6]])
+                conn.commit() 
+                cursor.execute("SELECT value FROM counters WHERE counter_name = ?", [row[6]])
+                contract_load_number = str(cursor.fetchone()[0]).zfill(5)
 
 
-        cursor.execute("SELECT * FROM credentials WHERE CardID=?", [int(id)]) 
-        row = cursor.fetchone()
+                # Fill Report Data
+                report_data = {
+                'source_name': row[1],
+                'source_address': row[2],
+                'source_address_2': row[3],
+                'source_phone': row[4],
+                'ticket_number': ticket_number,
+                'date' : datetime.now().strftime('%B %d, %Y %I:%M %p'),
+                'commodity': row[5],
+                'contract_number' : row[6],
+                'load_number' : contract_load_number,
+                'customer_name': row[7],
+                'customer_address': row[8],
+                'customer_address_2': row[9],
+                'customer_city_state': "", #row[10], # TODO: Determine if this is needed
+                'carrier_name' : row[11],
+                'truck_plate' : row[12],
+                'trailer_plate' : row[13]
+                }
 
-        if row: # Ensures card is on file
-            # Update & Retrieve the Ticket Counter
-            cursor.execute("UPDATE counters SET value = value + 1 WHERE counter_name = 'loadID'") # TODO: Rename loadID to ticket
-            conn.commit() 
-            cursor.execute("SELECT value FROM counters WHERE counter_name = 'loadID';")
-            ticket_number = str(cursor.fetchone()[0]).zfill(5)
+                # Generate the report
+                output_file = report_path + f"Report_{datetime.now().strftime('%Y%m%d')}_{ticket_number}.pdf"
+                genPDF.generate_report(output_file, report_data)
 
-            # Update & Retrieve the Contract Load Counter
-            # TODO: initialize contract counters on startup
-            cursor.execute('''INSERT OR IGNORE INTO counters
-                            VALUES (?, 0)''', [row[6]])
-            conn.commit() 
+                # Print the report
+                os.system(f"lp -d HP_Officejet_Pro_8600 {output_file}")
 
-            cursor.execute("UPDATE counters SET value = value + 1 WHERE counter_name = ?", [row[6]])
-            conn.commit() 
-            cursor.execute("SELECT value FROM counters WHERE counter_name = ?", [row[6]])
-            contract_load_number = str(cursor.fetchone()[0]).zfill(5)
-
-
-            # Fill Report Data
-            report_data = {
-            'source_name': row[1],
-            'source_address': row[2],
-            'source_address_2': row[3],
-            'source_phone': row[4],
-            'ticket_number': ticket_number,
-            'date' : datetime.now().strftime('%B %d, %Y %I:%M %p'),
-            'commodity': row[5],
-            'contract_number' : row[6],
-            'load_number' : contract_load_number,
-            'customer_name': row[7],
-            'customer_address': row[8],
-            'customer_address_2': row[9],
-            'customer_city_state': "", #row[10], # TODO: Determine if this is needed
-            'carrier_name' : row[11],
-            'truck_plate' : row[12],
-            'trailer_plate' : row[13]
-            }
-
-            # Generate the report
-            output_file = report_path + f"Report_{datetime.now().strftime('%Y%m%d')}_{ticket_number}.pdf"
-            genPDF.generate_report(output_file, report_data)
-
-            # Print the report
-            os.system(f"lp -d HP_Officejet_Pro_8600 {output_file}")
-
-            # Log the report
-            with open(ticket_log, "a") as csv_file:
-                csv_writer = csv.writer(csv_file, delimiter=",")
-                csv_writer.writerow([datetime.now().strftime('%m/%d/%Y %I:%M:%S %p'), ticket_number, contract_load_number]+[ i for i in row]) #prints the titles in the top row
-        else: # Card is not on file, log the error 
-            pass
+                # Log the report
+                with open(ticket_log, "a") as csv_file:
+                    csv_writer = csv.writer(csv_file, delimiter=",")
+                    csv_writer.writerow([datetime.now().strftime('%m/%d/%Y %I:%M:%S %p'), ticket_number, contract_load_number]+[ i for i in row]) #prints the titles in the top row
+            else: # Card is not on file, log the error 
+                pass
 
     except Exception as e:
         pass
