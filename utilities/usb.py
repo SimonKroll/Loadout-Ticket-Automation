c1630d8226f7bf7dd84f02128bfbf97e02e63326
Simon Kroll <Simon.krollfarms@gmail.com>
2024-03-03 21:11:53 -0600

    Added Output folder

--- utilities/usb.py
+++ utilities/usb.py
@@ -22,6 +22,7 @@ try:
         drive_path = f"/dev/{drive_name}"  # Assuming the drive is mounted under /dev
 
         mount_path = "/home/pi/dev/Loadout-Ticket-Automation/usbmount/"
+        output_path = mount_path+"LoadOutTicket/"
         project_path = "/home/pi/dev/Loadout-Ticket-Automation/"
 
         #db_file = project_path+'db/LoadoutCreds.db'
@@ -31,17 +32,21 @@ try:
         # mount the drive
         os.system(f"mount {drive_path} {mount_path}")
 
+        #create folder
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+
         # Upload History log
         if os.path.isfile(load_hist_file):
-            os.system(f" cp {load_hist_file} {mount_path}")
+            os.system(f" cp {load_hist_file} {output_path}")
 
         #TODO: check for file and read it into DB
         conn = sqlite3.connect(db_file)
         cursor = conn.cursor()
-        logging.error("is File?  {}".format(os.path.isfile(mount_path+"card_data.csv")))
-        if os.path.isfile(mount_path+"card_data.csv"):
+        logging.error("is File?  {}".format(os.path.isfile(output_path+"card_data.csv")))
+        if os.path.isfile(output_path+"card_data.csv"):
             # Open and read the CSV file
-            with open(mount_path+'card_data.csv', 'r') as csv_file:
+            with open(output_path+'card_data.csv', 'r') as csv_file:
                 csv_reader = csv.reader(csv_file)
 
                 # Skip the header row
@@ -61,7 +66,7 @@ try:
             #TODO: When new data is added to DB, create new contract instance
         else:
             cursor.execute("select * from credentials")
-            with open(mount_path+"card_data.csv", "w") as csv_file:
+            with open(output_path+"card_data.csv", "w") as csv_file:
                 csv_writer = csv.writer(csv_file, delimiter=",")
                 csv_writer.writerow([i[0] for i in cursor.description]) #prints the titles in the top row
                 #csv_writer.writerows(cursor)
@@ -70,7 +75,7 @@ try:
                     row = (card_id,) + row[1:]
                     csv_writer.writerow(row)
 
-            dirpath = mount_path + "card_data.csv"
+            dirpath = output_path + "card_data.csv"
             logging.warning("Data exported Successfully into {}".format(dirpath))
 
 
